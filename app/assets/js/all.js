53b5df149481a670dfbf471b1e6264fe91d648bc
JHOUJHOU <a01450887@gmail.com>
2022-08-27 14:45:15 +0800

    圖片更新

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,9 @@
-$(function() {
-  console.log('Hello Bootstrap5');
+var swiper = new Swiper('.mySwiper', {
+  slidesPerView: 2,
+  slidesPerColumn: 2,
+  spaceBetween: 30,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
 });
