910f4d0705fa56d5955774da7fdf9f951fbe0818
JHOUJHOU <a01450887@gmail.com>
2022-08-27 23:55:59 +0800

    Update 2022-08-27T15:55:42.606Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,6 +1,18 @@
 "use strict";
 
-$(function () {
-  console.log('Hello Bootstrap5');
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  freeMode: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 40
+    }
+  }
 });
 //# sourceMappingURL=all.js.map
