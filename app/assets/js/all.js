b348507ccc694ed02322cf2e87af2882476e7b9f
JHOUJHOU <a01450887@gmail.com>
2022-08-27 21:46:22 +0800

    更新圖片

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,9 +1,15 @@
-var swiper = new Swiper('.mySwiper', {
-  slidesPerView: 2,
-  slidesPerColumn: 2,
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
   spaceBetween: 30,
+  freeMode: true,
   pagination: {
-    el: '.swiper-pagination',
+    el: ".swiper-pagination",
     clickable: true,
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+    },
   },
 });
